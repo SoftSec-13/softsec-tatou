22e56306091444352ab7ce0acb3a48271180144b
francescofronte <152978753+francescofronte@users.noreply.github.com>
2025-09-16 13:44:26 +0200

    Add upload doc test, touch up previous tests
    Implemented jsonify in previous routes

--- server/test/test_api.py
+++ server/test/test_api.py
@@ -1,4 +1,6 @@
 from server import app
+from flask import jsonify
+from datetime import datetime
 
 
 def test_healthz_route():
@@ -10,35 +12,54 @@ def test_healthz_route():
 
 def test_create_user_route():
     client = app.test_client()
-    json_parameters = {"login":"username", "password":"password", "email":"user@email.se"}
+    parameters = {"login":"username", "password":"password", "email":"user@email.se"}
+    json_parameters = jsonify(parameters)
     resp = client.post("/create-user", json=json_parameters)
 
     #basic tests
     assert resp.status_code == 201
     assert resp.is_json
-    #check types, check non empty
+    #check types
     assert isinstance(resp.get("id"), int)
     assert isinstance(resp.get("login"), str) 
     assert isinstance(resp.get("email"), str)
     #check values are what we submitted
-    assert resp.get("login") == json_parameters["login"]
-    assert resp.get("email") == json_parameters["email"]
+    assert resp.get("login") == parameters["login"]
+    assert resp.get("email") == parameters["email"]
 
 def test_login_route():
     client = app.test_client()
-    json_parameters = {"email":"user@email.se", "password":"password"}
+    parameters = {"email":"user@email.se", "password":"password"}
+    json_parameters = jsonify(parameters)
     resp = client.post("/login", json=json_parameters)
 
     #basic tests
     assert resp.status_code == 200
     assert resp.is_json
-    #check types, check non empty
+    #check types
     assert isinstance(resp.get("token"), str) 
     assert isinstance(resp.get("token_type"), str)
     assert isinstance(resp.get("expires_in"), int)
     #check val
     assert resp.get("token_type") == "bearer"
 
+def test_upload_document_route():
+    client = app.test_client()
+    pdf_file = "" #TODO add dummy file for testing
+    parameters = {"file":pdf_file, "name":"My File"}
+    resp = client.post("/login", files=parameters)
+
+    #basic tests
+    assert resp.status_code == 200
+    assert resp.is_json
+    #check types
+    assert isinstance(resp.get("id"), str) 
+    assert isinstance(resp.get("name"), str) 
+    assert isinstance(resp.get("creation"), str) and datetime.fromisoformat(resp.get("creation"))
+    assert isinstance(resp.get("sha256"), str) 
+    assert isinstance(resp.get("size"), int) 
+    #check value
+    assert resp.get("name") == parameters["name"]
 
 
 
