cd9cb4c5a0309f2a45c76ceb1cbb2ff3442047fa
Lyubo-IP <lyubomir.stoykov@ipercept.io>
2025-09-28 21:40:42 +0200

    Disable default watermarking methods

--- server/src/watermarking_utils.py
+++ server/src/watermarking_utils.py
@@ -33,11 +33,9 @@ import hashlib
 import re
 from typing import Any, Final
 
-from add_after_eof import AddAfterEOF
 from robust_xmp_watermark import RobustXmpWatermark
 from signed_annotation_watermark import SignedAnnotationWatermark
 from structural_and_overlay_watermark import StructuralOverlay
-from unsafe_bash_bridge_append_eof import UnsafeBashBridgeAppendEOF
 from watermarking_method import (
     PdfSource,
     WatermarkingMethod,
@@ -49,10 +47,10 @@ from watermarking_method import (
 # --------------------
 
 METHODS: dict[str, WatermarkingMethod] = {
-    AddAfterEOF.name: AddAfterEOF(),
+    # AddAfterEOF.name: AddAfterEOF(),
     RobustXmpWatermark.name: RobustXmpWatermark(),
     StructuralOverlay.name: StructuralOverlay(),
-    UnsafeBashBridgeAppendEOF.name: UnsafeBashBridgeAppendEOF(),
+    # UnsafeBashBridgeAppendEOF.name: UnsafeBashBridgeAppendEOF(),
     SignedAnnotationWatermark.name: SignedAnnotationWatermark(),
 }
 #: Registry of available watermarking methods.
