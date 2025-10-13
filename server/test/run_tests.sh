db03b193f0fe62d813ea66e5d99dc950560e91e7
francescofronte <152978753+francescofronte@users.noreply.github.com>
2025-10-13 19:31:04 +0200

    Shell scripts for testing

---
+++ server/test/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker compose down -v db
+docker compose up -d db
+
+#linux
+export DB_HOST=127.0.0.1
+export DB_USER=tatou
+export DB_PASSWORD=tatou
+export DB_NAME=tatou
+export TOKEN_TTL_SECONDS=3600
+
+sleep 5
+python -m pytest -vv test_api.py
